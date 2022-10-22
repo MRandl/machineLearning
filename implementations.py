95fdd8d2d54eb2dc9994039c69130f34fe73ec86
Mathis Randl <mathis.randl@epfl.ch>
2022-10-22 18:57:33 +0200

    added doc

--- implementations.py
+++ implementations.py
@@ -3,6 +3,15 @@ from random import randint
 
 
 def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
+    """
+    gradient descent using mse error
+    arguments :
+    y -- the true assignment
+    tx -- array of features for each datapoint
+    initial_w -- initial weights from which we start the GD
+    max_iters -- the amount of iterations we perform
+    gamma -- the GD step size 
+    """
     w = initial_w
     N = tx.shape[0]
 
@@ -18,6 +27,15 @@ def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
 
 
 def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
+    """
+    stochastic gradient descent using mse error
+    arguments :
+    y -- the true assignment
+    tx -- array of features for each datapoint
+    initial_w -- initial weights from which we start the SGD
+    max_iters -- the amount of iterations we perform
+    gamma -- the GD step size 
+    """
     w = initial_w
     N = tx.shape[0]
 
@@ -36,6 +54,12 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
 
 
 def least_squares(y, tx):
+    """
+    least squares linear regression solved with equations
+    arguments :
+    y -- the true assignment
+    tx -- array of features for each datapoint
+    """
     weights = np.linalg.inv(tx.T @ tx) @ tx.T @ y
 
     e = y - (tx @ weights)
@@ -45,6 +69,13 @@ def least_squares(y, tx):
 
 
 def ridge_regression(y, tx, lambda_):
+    """
+    ridge regression solved with equations
+    arguments :
+    y -- the true assignment
+    tx -- array of features for each datapoint
+    lambda_ -- the penalization term
+    """
     N, O = tx.shape
     ridge_matrix = (2 * N * lambda_) * np.eye(O)
     weights = np.linalg.inv(tx.T @ tx + ridge_matrix) @ tx.T @ y
@@ -60,6 +91,15 @@ def sigmoid(z):
 
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    """
+    logistic regression solved with GD
+    arguments :
+    y -- the true assignment
+    tx -- array of features for each datapoint
+    initial_w -- initial weights from which we start the GD
+    max_iters -- the amount of iterations we perform
+    gamma -- the step size
+    """
     w = initial_w
     N = tx.shape[0]
 
@@ -74,6 +114,16 @@ def logistic_regression(y, tx, initial_w, max_iters, gamma):
 
 
 def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
+    """
+    regularized logistic regression solved with GD
+    arguments :
+    y -- the true assignment
+    tx -- array of features for each datapoint
+    lambda_ -- the penalization term
+    initial_w -- initial weights from which we start the GD
+    max_iters -- the amount of iterations we perform
+    gamma -- the step size
+    """
     w = initial_w
     N = tx.shape[0]
 
